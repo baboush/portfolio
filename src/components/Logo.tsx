095e780b838aea511693e3a0bb85fc32d6143071
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-08-27 15:53:35 +0200

    update(general): update protected route dashboard
    update logo component redirect home page
    update router about contact home page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const Logo = () => {
-  return <img src="/public/logo.svg" alt="Logo" width={180} height={180} />;
+  return (
+    <a href="/">
+      <img src="/public/logo.svg" alt="Logo" width={180} height={180} />
+    </a>
+  );
 };
 
 export default Logo;
